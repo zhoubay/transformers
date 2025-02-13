1dfd918c0d0c265ccdd087d98d5f979e917ca127
Xibin Bayes Zhou <zhouxibin@westlake.edu.cn>
2025-02-13 12:45:32 +0800

    pass all EvollaModelTest test and remove processor test

--- tests/models/evolla/test_modeling_evolla.py
+++ tests/models/evolla/test_modeling_evolla.py
@@ -21,6 +21,7 @@ import pytest
 from pytest import mark
 from parameterized import parameterized
 import tempfile
+import numpy as np
 
 from transformers import BitsAndBytesConfig, EvollaConfig, is_torch_available, is_vision_available
 from transformers.testing_utils import (
@@ -44,7 +45,7 @@ from transformers import EsmTokenizer, LlamaTokenizerFast
 
 from ...generation.test_utils import GenerationTesterMixin
 from ...test_configuration_common import ConfigTester
-from ...test_modeling_common import ModelTesterMixin, floats_tensor, ids_tensor, random_attention_mask, _config_zero_init
+from ...test_modeling_common import ModelTesterMixin, floats_tensor, ids_tensor, random_attention_mask, _config_zero_init, is_torch_fp16_available_on_device, is_torch_bf16_available_on_device, set_model_tester_for_less_flaky_test, set_config_for_less_flaky_test, set_model_for_less_flaky_test, sdpa_kernel
 from ...test_pipeline_mixin import PipelineTesterMixin
 
 
@@ -186,7 +187,7 @@ class EvollaModelTester:
         image_attention_mask,
         interpolate_pos_encoding,
     ):
-        model = EvollaForVisionText2Text(config)
+        model = EvollaForProteinText2Text(config)
         model.to(torch_device)
         model.eval()
         model.generate(
@@ -214,14 +215,11 @@ class EvollaModelTester:
             "protein_attention_mask": protein_input_mask,
         }
         return config, inputs_dict
+
 @require_torch
 class EvollaModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestCase):
     all_model_classes = (EvollaModel, EvollaForProteinText2Text) if is_torch_available() else ()
-    pipeline_model_mapping = (
-        {"feature-extraction": EvollaModel}
-        if is_torch_available()
-        else {}
-    )
+    pipeline_model_mapping = {"feature-extraction": EvollaModel} if is_torch_available() else {}
     test_pruning = False
     test_headmasking = False
     test_torchscript = False
@@ -230,15 +228,14 @@ class EvollaModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestCase):
     def setUp(self):
         self.model_tester = EvollaModelTester(self)
         self.config_tester = ConfigTester(self, config_class=EvollaConfig, hidden_size=37)
-        self.is_encoder_decoder = self.model_tester.is_encoder_decoder
-        # protein_tokenizer = EsmTokenizer.from_pretrained("/zhouxibin/workspaces/ProteinQA/Models/SaProt_35M_AF2")
-        # tokenizer = LlamaTokenizerFast.from_pretrained("/zhouxibin/workspaces/ProteinQA/Models/meta-llama_Meta-Llama-3-8B-Instruct")
-        # self.processor = EvollaProcessor(protein_tokenizer, tokenizer)
-        self.processor = EvollaProcessor.from_pretrained("/zhouxibin/workspaces/transformers/src/transformers/models/evolla/evolla-base")
 
+    @property
+    def is_encoder_decoder(self):
+        return self.model_tester.is_encoder_decoder
+    
     def _prepare_for_class(self, inputs_dict, model_class, return_labels=False):
         inputs_dict = super()._prepare_for_class(inputs_dict, model_class, return_labels=return_labels)
-        # XXX: IdeficsForVisionText2TextTest has no MODEL_FOR group yet, but it should be the same
+        # XXX: EvollaForProteinText2Text has no MODEL_FOR group yet, but it should be the same
         # as MODEL_FOR_CAUSAL_LM_MAPPING_NAMES, so for now manually changing to do the right thing
         # as super won't do it
         if return_labels:
@@ -256,39 +253,6 @@ class EvollaModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestCase):
             super().test_model_outputs_equivalence()
         finally:
             self.all_model_classes = orig
-
-    def prepare_input_and_expected_output(self):
-        amino_acid_sequence = "AAAA"
-        foldseek_sequence = "dddd"
-        question = "What is the function of this protein?"
-        answer = "I don't know"
-
-        expected_output = {
-            "protein_input_ids": torch.tensor([[0, 13, 13, 13, 13, 2]]),
-            "protein_attention_mask": torch.tensor([[1, 1, 1, 1, 1, 1]]),
-            "text_input_ids": torch.tensor([[128000, 128006,   9125, 128007,    271,   2675,    527,    459,  15592,
-            6335,    430,    649,   4320,    904,   4860,    922,  13128,     13,
-          128009, 128006,    882, 128007,    271,   3923,    374,    279,    734,
-             315,    420,  13128,     30, 128009, 128006,  78191, 128007,    271]]),
-            "text_attention_mask": torch.tensor([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]),
-        }
-        protein_dict = {
-            "aa_seq": amino_acid_sequence,
-            "foldseek": foldseek_sequence
-        }
-        message = [{"role": "system", "content": "You are an AI expert that can answer any questions about protein."},
-                   {"role": "user", "content": question}]
-        return protein_dict, message, expected_output
-
-    def test_processor(self):
-        protein_dict, message, expected_output = self.prepare_input_and_expected_output()
-        inputs = self.processor(proteins=[protein_dict],
-                                messages_list=[message])
-        
-        # check if the input is correct
-        for key, value in expected_output.items():
-            self.assertTrue(torch.equal(inputs[key], value), f"inputs[key] is {inputs[key]} and expected_output[key] is {expected_output[key]}")
 
     def test_saprot_output(self):
         config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
@@ -337,62 +301,6 @@ class EvollaModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestCase):
                 outputs = model(**self._prepare_for_class(inputs_dict, model_class))
             print(outputs)
 
-
-    # def test_attention_outputs(self):
-    #     config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
-    #     config.return_dict = True
-    #     seq_len = getattr(self.model_tester, "seq_length", None)
-    #     encoder_seq_length = getattr(self.model_tester, "encoder_seq_length", seq_len)
-    #     encoder_key_length = getattr(self.model_tester, "key_length", encoder_seq_length)
-
-    #     for model_class in self.all_model_classes:
-    #         inputs_dict["output_attentions"] = True
-    #         inputs_dict["output_hidden_states"] = False
-    #         config.return_dict = True
-    #         model = model_class(config)
-    #         model.to(torch_device)
-    #         model.eval()
-    #         with torch.no_grad():
-    #             outputs = model(**self._prepare_for_class(inputs_dict, model_class))
-    #         attentions = outputs.attentions
-
-    #         self.assertEqual(len(attentions), self.model_tester.num_hidden_layers)
-
-    #         # check that output_attentions also work using config
-    #         del inputs_dict["output_attentions"]
-    #         config.output_attentions = True
-    #         model = model_class(config)
-    #         model.to(torch_device)
-    #         model.eval()
-    #         with torch.no_grad():
-    #             outputs = model(**self._prepare_for_class(inputs_dict, model_class))
-    #         attentions = outputs.attentions
-    #         self.assertListEqual(
-    #                 list(attentions[0].shape[-3:]),
-    #                 [self.model_tester.num_attention_heads, encoder_seq_length, encoder_key_length],
-    #             )
-    #         out_len = len(outputs)
-
-    #         # Check attention is always last and order is fine
-    #         inputs_dict["output_attentions"] = True
-    #         inputs_dict["output_hidden_states"] = True
-    #         model = model_class(config)
-    #         model.to(torch_device)
-    #         model.eval()
-    #         with torch.no_grad():
-    #             outputs = model(**self._prepare_for_class(inputs_dict, model_class))
-
-    #         self.assertEqual(out_len + 2, len(outputs))
-
-    #         self_attentions = outputs.encoder_attentions if config.is_encoder_decoder else outputs.attentions
-
-    #         self.assertEqual(len(self_attentions), self.model_tester.num_hidden_layers)
-
-    #         self.assertListEqual(
-    #             list(self_attentions[0].shape[-3:]),
-    #             [self.model_tester.num_attention_heads, encoder_seq_length, encoder_key_length],
-    #         )
-
     def test_initialization(self):
         # we skip the latents initialization test
         config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
@@ -417,61 +325,6 @@ class EvollaModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestCase):
     @unittest.skip("Evolla requires both text and protein inputs which is currently not done in this test.")
     def test_eager_matches_sdpa_inference(self):
         pass
-
-    # @unittest.skip(reason="We cannot configure to output a smaller model.")
-    # def test_model_is_small(self):
-    #     pass
-
-    @unittest.skip(reason="Hard to find out what's wrong with this test. The error is AssertionError: device(type='cpu') != device(type='meta')")
-    def test_disk_offload_bin(self):
-        pass
-
-    @unittest.skip(reason="Hard to find out what's wrong with this test. The error is AssertionError: device(type='cpu') != device(type='meta')")
-    def test_disk_offload_safetensors(self):
-        pass
-
-    @require_accelerate
-    @mark.accelerate_tests
-    @require_torch_gpu
-    def test_cpu_offload(self):
-        config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
-
-        for model_class in self.all_model_classes:
-            if model_class._no_split_modules is None:
-                continue
-
-            inputs_dict_class = self._prepare_for_class(inputs_dict, model_class)
-            model = model_class(config).eval()
-            model = model.to(torch_device)
-
-            torch.manual_seed(0)
-            # modify a little bit with torch.no_grad() to avoid memory leak
-            with torch.no_grad():
-                base_output = model(**inputs_dict_class)
-
-            model_size = compute_module_sizes(model)[""]
-            # We test several splits of sizes to make sure it works.
-            max_gpu_sizes = [int(p * model_size) for p in self.model_split_percents[1:]]
-            with tempfile.TemporaryDirectory() as tmp_dir:
-                model.cpu().save_pretrained(tmp_dir)
-
-                for max_size in max_gpu_sizes:
-                    max_memory = {0: max_size, "cpu": model_size * 2}
-                    new_model = model_class.from_pretrained(tmp_dir, device_map="auto", max_memory=max_memory)
-                    # Making sure part of the model will actually end up offloaded
-                    self.assertSetEqual(set(new_model.hf_device_map.values()), {0, "cpu"})
-
-                    self.check_device_map_is_respected(new_model, new_model.hf_device_map)
-
-                    torch.manual_seed(0)
-                    # modify a little bit with torch.no_grad() to avoid memory leak
-                    with torch.no_grad():
-                        new_output = new_model(**inputs_dict_class)
-
-                    if isinstance(base_output[0], tuple) and isinstance(new_output[0], tuple):
-                        self.assertTrue(torch.allclose(a, b, atol=1e-5) for a, b in zip(base_output[0], new_output[0]))
-                    else:
-                        self.assertTrue(torch.allclose(base_output[0], new_output[0], atol=1e-5))
 
 @require_torch
 @require_vision
@@ -515,7 +368,7 @@ class EvollaModelIntegrationTest(TestCasePlus):
             load_in_4bit=True,
             bnb_4bit_compute_dtype="float16",
         )
-        model = EvollaForVisionText2Text.from_pretrained(
+        model = EvollaForProteinText2Text.from_pretrained(
             "westlake-repl/Evolla-10B", quantization_config=quantization_config, device_map="auto"
         )
         processor = self.default_processor
